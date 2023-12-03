754344980ee5d2f3e7f8b907ce1e2e2046990157
Elena Klepova <elena.klyopova@gmail.com>
2023-12-03 14:19:28 +0300

    added soft assertions and endpoints

---
+++ src/test/java/petstore/enpoints/Endpoints.java
@@ -0,0 +1,5 @@
+package petstore.enpoints;
+
+public final class Endpoints {
+    public static final String pet = "/pet/{id}";
+}
--- src/test/java/petstore/tests/PetstoreTests.java
+++ src/test/java/petstore/tests/PetstoreTests.java
@@ -1,11 +1,13 @@
 package petstore.tests;
 
 import io.qameta.allure.*;
+import org.assertj.core.api.SoftAssertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Tag;
 import org.junit.jupiter.api.Tags;
 import org.junit.jupiter.api.Test;
 import petstore.data.TestData;
+import petstore.enpoints.Endpoints;
 import petstore.models.*;
 
 import static io.qameta.allure.Allure.step;
@@ -17,9 +19,10 @@ import static petstore.specs.Specs.*;
 @Feature("Pets management")
 @Tags({@Tag("api"), @Tag("pet")})
 
-public class PetstoreTests extends TestBase{
+public class PetstoreTests extends TestBase {
 
     TestData testData = new TestData();
+    Endpoints endpoints = new Endpoints();
 
     @Test
     @DisplayName("Testing of successful pet creation")
@@ -33,18 +36,20 @@ public class PetstoreTests extends TestBase{
         pet.setStatus(testData.status);
 
         CreatePetResponseModel response = step("Create a new pet", () ->
-        given(requestSpec)
-                .body(pet)
-                .when()
-                .post("/pet")
-                .then()
-                .spec(createPetResponse200Spec)
-                .extract().as(CreatePetResponseModel.class));
+                given(requestSpec)
+                        .body(pet)
+                        .when()
+                        .post(endpoints.pet, "")
+                        .then()
+                        .spec(createPetResponse200Spec)
+                        .extract().as(CreatePetResponseModel.class));
 
         step("Verify response", () -> {
-            assertThat(response.getId()).isEqualTo(55);
-            assertThat(response.getName()).isEqualTo(testData.name);
-            assertThat(response.getStatus()).isEqualTo(testData.status);
+            SoftAssertions.assertSoftly(softly -> {
+                softly.assertThat(response.getId()).isEqualTo(55);
+                softly.assertThat(response.getName()).isEqualTo(testData.name);
+                softly.assertThat(response.getStatus()).isEqualTo(testData.status);
+            });
         });
     }
 
@@ -57,17 +62,19 @@ public class PetstoreTests extends TestBase{
         successfulAddPetTest();
 
         CreatePetResponseModel response = step("Get an existing pet", () ->
-        given(requestSpec)
-                .when()
-                .get("/pet/55")
-                .then()
-                .spec(createPetResponse200Spec)
-                .extract().as(CreatePetResponseModel.class));
+                given(requestSpec)
+                        .when()
+                        .get(endpoints.pet, 55)
+                        .then()
+                        .spec(createPetResponse200Spec)
+                        .extract().as(CreatePetResponseModel.class));
 
         step("Verify response", () -> {
-            assertThat(response.getId()).isEqualTo(55);
-            assertThat(response.getName()).isEqualTo(testData.name);
-            assertThat(response.getStatus()).isEqualTo(testData.status);
+            SoftAssertions.assertSoftly(softly -> {
+                softly.assertThat(response.getId()).isEqualTo(55);
+                softly.assertThat(response.getName()).isEqualTo(testData.name);
+                softly.assertThat(response.getStatus()).isEqualTo(testData.status);
+            });
         });
     }
 
@@ -81,17 +88,19 @@ public class PetstoreTests extends TestBase{
 
 
         DeletePetResponseModel response = step("Delete an existing pet", () ->
-        given(requestSpec)
-                .when()
-                .delete("/pet/55")
-                .then()
-                .spec(createPetResponse200Spec)
-                .extract().as(DeletePetResponseModel.class));
+                given(requestSpec)
+                        .when()
+                        .delete(endpoints.pet, 55)
+                        .then()
+                        .spec(createPetResponse200Spec)
+                        .extract().as(DeletePetResponseModel.class));
 
         step("Verify response", () -> {
-            assertThat(response.getCode()).isEqualTo(200);
-            assertThat(response.getMessage()).isEqualTo("55");
-            assertThat(response.getType()).isEqualTo("unknown");
+            SoftAssertions.assertSoftly(softly -> {
+                softly.assertThat(response.getCode()).isEqualTo(200);
+                softly.assertThat(response.getMessage()).isEqualTo("55");
+                softly.assertThat(response.getType()).isEqualTo("unknown");
+            });
         });
     }
 
@@ -102,11 +111,11 @@ public class PetstoreTests extends TestBase{
     void unsuccessfulDeletePetTest() {
 
         step("Delete a pet that does not exist", () ->
-        given(requestSpec)
-                .when()
-                .delete("/pet/550")
-                .then()
-                .spec(response404Spec));
+                given(requestSpec)
+                        .when()
+                        .delete(endpoints.pet, 550)
+                        .then()
+                        .spec(response404Spec));
     }
 
     @Test
@@ -116,17 +125,19 @@ public class PetstoreTests extends TestBase{
     void unsuccessfulGetPetTest() {
 
         GetPetResponseModel response = step("Get a pet that does not exist", () ->
-        given(requestSpec)
-                .when()
-                .get("pet/404")
-                .then()
-                .spec(response404Spec)
-                .extract().as(GetPetResponseModel.class));
+                given(requestSpec)
+                        .when()
+                        .get(endpoints.pet, 404)
+                        .then()
+                        .spec(response404Spec)
+                        .extract().as(GetPetResponseModel.class));
 
         step("Verify response", () -> {
-            assertThat(response.getCode()).isEqualTo(1);
-            assertThat(response.getMessage()).isEqualTo("Pet not found");
-            assertThat(response.getType()).isEqualTo("error");
+            SoftAssertions.assertSoftly(softly -> {
+                softly.assertThat(response.getCode()).isEqualTo(1);
+                softly.assertThat(response.getMessage()).isEqualTo("Pet not found");
+                softly.assertThat(response.getType()).isEqualTo("error");
+            });
         });
     }
 }
